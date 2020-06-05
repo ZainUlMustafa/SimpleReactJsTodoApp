7b438d31c2d6f9554a6aea837ad8d46f211dbb82
ZainUlMustafa <zain2984@gmail.com>
2020-06-05 21:40:05 +0500

    corrected svg

--- todoapp/src/components/sketchit/SketchPage.js
+++ todoapp/src/components/sketchit/SketchPage.js
@@ -9,18 +9,25 @@ class SketchPage extends Component {
 
         this.state = {
             customSvgs: [
-                <circle cx="20" cy="20" r="20" className="customShape" onClick={() => {this.handleShapeOnClick(0)}} key={0}/>,
-                <circle cx="100" cy="20" r="20" className="customShape" onClick={() => {this.handleShapeOnClick(1)}}  key={1}/>,
-                <rect x="30" y="50" width="100" height="100" className="customShape" onClick={() => {this.handleShapeOnClick(2)}}  key={2}/>,
-            ]
+                <circle cx="120" cy="500" r="20" className="customShape" onClick={() => { this.handleShapeOnClick(0) }} key={0} onDoubleClick={() => {this.handleShapeOnDoubleClick(0)}}/>,
+                <circle cx="600" cy="200" r="50" className="customShape" onClick={() => { this.handleShapeOnClick(1) }} key={1} onDoubleClick={() => {this.handleShapeOnDoubleClick(1)}}/>,
+                <rect x="0" y="0" width="100" height="200" className="customShape" onClick={() => { this.handleShapeOnClick(2) }} key={2} onDoubleClick={() => {this.handleShapeOnDoubleClick(2)}}/>,
+            ],
+
+            imgWidth: null,
+            imgHeight: null,
+            svgWidth: null,
+            svgHeight: null,
+
+            zoomFactor: 1,
         }
     }
 
     handleAddShape = (sda, ind) => {
         const id = Math.random();
-        const svgSquareShape = <rect x={sda[0]} y={sda[1]} width={sda[2]} height={sda[2]} className="customShape" onClick={() => {this.handleShapeOnClick(id)}} key={id}/>;
-        const svgCircleShape = <circle cx={sda[0]} cy={sda[1]} r={sda[2]} className="customShape" onClick={() => {this.handleShapeOnClick(id)}} key={id}/>;
-        const svgRectShape = <rect x={sda[0]} y={sda[1]} width={sda[2]} height={sda[3]} className="customShape" onClick={() => {this.handleShapeOnClick(id)}} key={id}/>;
+        const svgSquareShape = <rect x={sda[0]} y={sda[1]} width={sda[2]} height={sda[2]} className="customShape" onClick={() => { this.handleShapeOnClick(id) }} key={id} />;
+        const svgCircleShape = <circle cx={sda[0]} cy={sda[1]} r={sda[2]} className="customShape" onClick={() => { this.handleShapeOnClick(id) }} key={id} />;
+        const svgRectShape = <rect x={sda[0]} y={sda[1]} width={sda[2]} height={sda[3]} className="customShape" onClick={() => { this.handleShapeOnClick(id) }} key={id} />;
 
         const svgShape = ind === 0 ? svgRectShape : ind === 1 ? svgCircleShape : svgSquareShape
         let customSvgs = [...this.state.customSvgs, svgShape];
@@ -33,6 +40,22 @@ class SketchPage extends Component {
 
     handleShapeOnClick = (id) => {
         console.log("shape id => " + id);
+        $("#addShapeModal").modal("show");
+    }
+
+    handleShapeOnDoubleClick = (id) => {
+        console.log("open editing modal => " + id);
+    }
+
+    onImgLoad = ({ target: img }) => {
+        this.setState({
+            imgWidth: img.width,
+            imgHeight: img.height,
+            svgWidth: img.width,
+            svgHeight: img.height,
+        })
+
+        console.log(img.width)
     }
 
     render() {
@@ -41,16 +64,20 @@ class SketchPage extends Component {
         return (
             <div className="my-2 mx-5">
                 <div className="text-center">
-                        <button type="button" className="btn btn-outline-dark" data-toggle="modal" data-target="#addShapeModal">+ Add a shape</button>
+                    <button type="button" className="btn btn-outline-dark" data-toggle="modal" data-target="#addShapeModal">+ Add a shape</button>
                 </div>
 
                 {/* CANVAS */}
                 <div className="sketch-container text-center">
                     <div className="sketch-box">
-                        <img className="sketch-img" src={Floor} width="1000" height="1000" alt="React Logo" />
+                        <img className="sketch-img" src={Floor} alt="React Logo" onLoad={this.onImgLoad} width={this.state.imgWidth} height={this.state.imgHeight} />
                     </div>
                     <div className="sketch-box sketch-stack-top">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="1000" height="1000">
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            width={this.state.svgWidth}
+                            height={this.state.svgHeight}
+                        >
                             {customSvgs}
                         </svg>
                     </div>
@@ -67,7 +94,7 @@ class SketchPage extends Component {
                                 </button>
                             </div>
                             <div className="modal-body">
-                                <AddShape handleAddShape={this.handleAddShape}/>
+                                <AddShape handleAddShape={this.handleAddShape} />
                             </div>
                         </div>
                     </div>
